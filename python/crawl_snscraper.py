5519c3c6e699f0c4d939f7b0f85b487d817a2687
root <root@master1.localhost>
2020-12-17 21:45:10 +0900

    add others

--- python/crawl_snscraper.py
+++ python/crawl_snscraper.py
@@ -1,23 +1,23 @@
+# -*- coding: utf-8 -*-
+
 import snscrape.modules.twitter as sntwitter
 import csv
-import timeit
+import sys
 
-keyword = '코로나'
-month = 3
+fir_arg = sys.argv[1]
+sec_arg = sys.argv[2]
+sec_arg = int(sec_arg)
 
-start_time = timeit.default_timer()
+keyword = fir_arg
+month = sec_arg
 
-#Open/create a file to append data to
+# Open/create a file to append data to
 csvFile = open( str(month) + '_' + keyword + '.csv', 'a', newline='', encoding='utf8')
 
-#Use csv writer
+# Use csv writer
 csvWriter = csv.writer(csvFile)
 csvWriter.writerow(['id','date','tweet']) 
 
 for i,tweet in enumerate(sntwitter.TwitterSearchScraper(keyword + 'since:2020-' + str(month) +'-01 until:2020-' + str(month + 1) + '-01 -filter:links -filter:replies').get_items()) :
     csvWriter.writerow([tweet.id, tweet.date, tweet.content])
 csvFile.close()
-
-end_time = timeit.default_timer()
-
-print("%f s!!" % (end_time - start_time))
--- python/daum_webpage_crawling.py
+++ python/daum_webpage_crawling.py
@@ -1,101 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import requests
-
-
-# In[2]:
-
-
 import json
-
-
-# In[3]:
-
-
 import urllib
-
-
-# In[4]:
-
-
 import pandas as pd
-
-
-# In[5]:
-
-
 from pandas import DataFrame
 
-
-# In[6]:
-
-
-api_key = "4d6aca65473af926115dec15546b1228"
-
-
-# In[7]:
+api_key = <<api_key>>
 
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
 
-
-# In[8]:
-
-
 session = requests.Session()
-
-
-# In[9]:
-
-
 session.headers.update( {'User-agent': user_agent, 'referer': None, 'Authorization': 'KakaoAK ' + api_key} )
 
-
-# In[10]:
-
-
 url_tpl = "https://dapi.kakao.com/v2/search/web"
-
-
-# In[11]:
-
-
-wanted_word = "코로나"
-
-
-# In[12]:
-
+wanted_word = "covid"
 
 page = 1
-
-
-# In[13]:
-
-
 size = 50
-
-
-# In[14]:
-
 
 content_list = []
     
 for i in range(0,6):
     page = i + 1      # 1~6까지가 된다.
     
-    # API에 전달할 파라미터 인코딩
     params = {"page": page, "query": wanted_word, "size": size}
     query = urllib.parse.urlencode(params)
     
-    # 최종 접속 주소 구성
     api_url = url_tpl + "?" + query
     api_url
     
-    # API에 접근하여 데이터 가져오기
     r = session.get(api_url)
 
     if r.status_code != 200:
@@ -114,41 +50,18 @@ for i in range(0,6):
     
 content_list
 
-
-# In[15]:
-
-
 text = " ".join(content_list)
 text
 
 
-# In[16]:
-
-
+# other jupyter file
 from konlpy.tag import Okt
-
-
-# In[20]:
-
-
 from collections import Counter
-
-
-# In[17]:
 
 
 nlp = Okt()
 
-
-# In[18]:
-
-
 nouns = nlp.nouns(text)
-nouns
-
-
-# In[19]:
-
 
 words = []
 
@@ -158,17 +71,6 @@ for n in nouns:
         
 words
 
-
-# In[21]:
-
-
 count = Counter(words)
 most = count.most_common(100)
 most
-
-
-# In[ ]:
-
-
-
-
